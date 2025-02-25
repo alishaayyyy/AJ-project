d186e82a91b61b5c07d531127296584d6051c46a
Jeiya Kumari <jeiyakumari@gmail.com>
2025-02-26 01:52:41 +0500

    one

--- js/app.js
+++ js/app.js
@@ -1,21 +1,5 @@
 import { auth ,initializeApp, getFirestore, collection, addDoc, doc, setDoc, getDoc,firebaseConfig} from './auth.js'; // Import Firebase's getAuth to get the current user
 
-function viewRecipe(url) {
-  window.location.href = url; 
-}
-
-
-function toggleWishlist(event, btn) {
-  event.stopPropagation(); 
-  const icon = btn.querySelector('i');
-  if (icon.classList.contains('far')) {
-    icon.classList.remove('far');
-    icon.classList.add('fas');
-  } else {
-    icon.classList.remove('fas');
-    icon.classList.add('far');
-  }
-}
 
 // user login or not
 // user login or not
